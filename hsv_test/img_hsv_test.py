4b41fb36178b7e2c5d24ba27550caf4eef4fd6f0
Tuna Uygun <uyguntuna2018@gmail.com>
2019-10-26 15:53:27 -0400

    Added 2019 Vision Images and image hsv tester

---
+++ hsv_test/img_hsv_test.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    # any operation
+    pass
+
+# Read the image
+img = cv2.imread("RocketPanelStraightDark36in.jpg", 1)
+print(img)
+
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L-H", "Trackbars", 0, 180, nothing)
+cv2.createTrackbar("L-S", "Trackbars", 66, 255, nothing)
+cv2.createTrackbar("L-V", "Trackbars", 134, 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", 180, 180, nothing)
+cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", 243, 255, nothing)
+
+while True:
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("L-H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L-S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L-V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U-H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U-S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U-V", "Trackbars")
+
+    lower_red = np.array([l_h, l_s, l_v])
+    upper_red = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    cv2.imshow("Img", img)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
+
